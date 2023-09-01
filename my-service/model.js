bcc7c108d9e81d3e6e4a01728e7b81b9f5979d99
Ibrahim Banat <Ibrahim.banat@altibbi.com>
2023-09-01 17:16:25 +0300

    implement restful with fastify

--- my-service/model.js
+++ my-service/model.js
@@ -1,23 +1,25 @@
-"use strict";
+'use strict'
 
 module.exports = {
     bicycle: bicycleModel()
 }
 
-function bicycleModel() {
+function bicycleModel () {
     const db = {
         1: { brand: 'Veloretti', color: 'green' },
         2: { brand: 'Batavus', color: 'yellow' }
     }
+
     return {
         read
     }
+
     function read (id, cb) {
         if (!(db.hasOwnProperty(id))) {
-            const err = Error('not found');
-            setImmediate(() => cb(err));
-            return;
+            const err = Error('not found')
+            setImmediate(() => cb(err))
+            return
         }
-        setImmediate(() => cb(null, db[id]));
+        setImmediate(() => cb(null, db[id]))
     }
 }
